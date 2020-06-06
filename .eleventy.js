7fbc664f1289c99c15337ed6c10e5db6e4dd9dde
MatthewNgan <58377002+MatthewNgan@users.noreply.github.com>
2020-06-06 15:49:03 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,19 @@
 module.exports = function(eleventyConfig) {
-      // Folders to copy into output.
+    // Allow data cascading instead of replacing. Mainly for post tags.
+    eleventyConfig.setDataDeepMerge(true);
+    // Folders to copy into output.
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("css");
-
     // If we use Netlify and has the _redirects file.
     eleventyConfig.addPassthroughCopy("_redirects");
     
     eleventyConfig.addFilter("toISOString", function(date) {
         return date.toISOString().split('T')[0];
     });
+    module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addCollection("lastThreeTasks", function(collection) {
+        return collection.getFilteredByTag('task').slice().reverse().slice(0, 3)
+    });
+};
 }
